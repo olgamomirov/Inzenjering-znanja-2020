af63c4044486f2783bfdd922c8c5984e23c2a3d2
Jovana <Jovana@192.168.56.1>
2020-05-25 19:53:50 +0200

    dopuna csv

--- Radiologija/konkretanPregled.pl
+++ Radiologija/konkretanPregled.pl
@@ -2,3 +2,5 @@ anamneza(olga_momirov,[otecena_noga]).
 pregled(olga_momirov,[prosirene_spoljasnje_vene]).
 anamneza(grfq,[bezbolan_cvor_na_dojci]).
 pregled(grfq,[vidljiva_zvezdasta_nehomogena_neostro_ogranicena_zona]).
+anamneza(sxfgi,[otecena_noga,topla_noga]).
+pregled(sxfgi,[prosirene_spoljasnje_vene]).
